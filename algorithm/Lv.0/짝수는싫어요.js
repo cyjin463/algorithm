1a6a20ec63877038b39a15e34b714a456c5e9fdb
cyjin463 <cyjin463@hanmail.net>
2022-12-12 22:58:46 +0900

    주석 처리

--- algorithm/Lv.0/짝수는싫어요.js
+++ algorithm/Lv.0/짝수는싫어요.js
@@ -1,21 +1,23 @@
-정수 n이 매개변수로 주어질 때,
+/*
+수 n이 매개변수로 주어질 때,
 n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+*/
 
 function solution(n) {
   var answer = [];
-  for (let i=0; n>=i; i++ ){
-      if(i%2!==0){
-          answer.push(i)
-      }
+  for (let i = 0; n >= i; i++) {
+    if (i % 2 !== 0) {
+      answer.push(i);
+    }
   }
-  return answer.sort((a,b) => a-b);
+  return answer.sort((a, b) => a - b);
 }
 
 //
 function solution(n) {
-    var answer = [];
+  var answer = [];
 
-    for (let i = 1; i<=n; i+=2) answer.push(i)
+  for (let i = 1; i <= n; i += 2) answer.push(i);
 
-    return answer;
+  return answer;
 }
