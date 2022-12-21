6b6d29b518fdc280837d5a9daed9cfcd1059a3e1
cyjin463 <cyjin463@hanmail.net>
2022-12-21 22:08:54 +0900

    가장큰수찾기

---
+++ algorithm/Lv.0/가장큰수찾기.js
@@ -0,0 +1,21 @@
+/**
+ 정수 배열 array가 매개변수로 주어질 때,
+ 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+ */
+
+ function solution(array) {
+  let newArray = [...array]
+  let max = 0
+  let index = 0
+  max = (newArray.sort((a,b) => b-a)[0])
+  index = array.indexOf(max)
+  return [max, index]
+}
+
+// 다른사람 문제 풀이
+
+function solution(array) {
+    let max = Math.max(...array);
+    return [max, array.indexOf(max)];
+}
+//Math.max 활용
