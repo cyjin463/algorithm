185ceca5ce94a6ab870872393764cae6886659da
cyjin463 <cyjin463@hanmail.net>
2022-12-21 22:13:18 +0900

    가장큰수찾기 리팩토링

--- algorithm/Lv.0/가장큰수찾기.js
+++ algorithm/Lv.0/가장큰수찾기.js
@@ -3,19 +3,26 @@
  가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
  */
 
- function solution(array) {
-  let newArray = [...array]
-  let max = 0
-  let index = 0
-  max = (newArray.sort((a,b) => b-a)[0])
-  index = array.indexOf(max)
-  return [max, index]
+function solution(array) {
+  let newArray = [...array];
+  let max = 0;
+  let index = 0;
+  max = newArray.sort((a, b) => b - a)[0];
+  index = array.indexOf(max);
+  return [max, index];
+}
+
+//리팩토링
+function solution(array) {
+  let max = Math.max(...array);
+  let index = array.indexOf(max);
+  return [max, index];
 }
 
 // 다른사람 문제 풀이
 
 function solution(array) {
-    let max = Math.max(...array);
-    return [max, array.indexOf(max)];
+  let max = Math.max(...array);
+  return [max, array.indexOf(max)];
 }
 //Math.max 활용
