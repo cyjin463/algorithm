353927a2575d45dd119089ff24220ac6d8dec79c
cyjin463 <cyjin463@hanmail.net>
2023-01-04 22:30:37 +0900

    숫자찾기

---
+++ algorithm/Lv.0/숫자찾기.js
@@ -0,0 +1,16 @@
+/*
+정수 num과 k가 매개변수로 주어질 때,
+num을 이루는 숫자 중에 k가 있으면 num의 그 숫자가 있는 자리 수를 return
+없으면 -1을 return 하도록 solution 함수를 완성해보세요.
+*/
+
+function solution(num, k) {
+    let answer = Array.from(String(num)).indexOf(String(k))
+    return answer === -1 ? -1 : answer+1
+}
+
+//다른 사람 문제 풀이
+
+function solution(num, k) {
+    return num.toString().split("").map((el) => Number(el)).indexOf(k) + 1 || -1
+}
